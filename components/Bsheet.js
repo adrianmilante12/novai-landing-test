181a480d9227f307cc2220b33d11377861e83dfa
Adrian Baido Milante <109199224+adrianmilante12@users.noreply.github.com>
2026-02-20 01:31:37 +0800

    Create Bsheet.js

---
+++ components/Bsheet.js
@@ -0,0 +1,23 @@
+export default function BottomSheet() {
+  return (
+    <div className="bottom-sheet" id="bottomSheet">
+      {/* Close / Grab Button */}
+      <div className="grab-btn" id="grabBtn"></div>
+
+      {/* Logo */}
+      <div className="sheet-logo">
+        <img src="/images/logo.png" alt="Logo" />
+      </div>
+
+      {/* Links */}
+      <div className="sheet-content">
+        <a href="#features">About</a>
+        <a href="showcase.html">Template</a>
+        <a href="#pricing">Pricing</a>
+        <a href="#faq">FAQ</a>
+      </div>
+
+      <a href="#get-started" className="btn-primary bottom-start">Get Started</a>
+    </div>
+  );
+}
