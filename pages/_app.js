a64af09e37e1d8bde9301260ec74f771522ee88d
Adrian Baido Milante <109199224+adrianmilante12@users.noreply.github.com>
2026-02-20 14:07:07 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
+// pages/_app.js
 import '../styles/globals.css';
+import GlobalScripts from '../components/GlobalScripts'; // <-- import the script wrapper
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalScripts />  {/* <-- runs all your scripts globally */}
+    </>
+  );
 }
